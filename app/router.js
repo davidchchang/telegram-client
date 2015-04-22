d609ce109d58240987d863197d04ecb0bdeac3e2
David Chang <dchang@nascentdigital.com>
2015-04-22 00:00:39 -0400

    Implement the hierarchy of routes for users and posts.

--- app/router.js
+++ app/router.js
@@ -6,4 +6,19 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
+  this.route('login', {
+    path: '/'
+  });
+  this.route('signup');
+  this.route('reset');
+  this.route('reset-success');
+  this.route('home');
+
+  this.resource('user', {
+    path: ':user_id'
+  }, function() {
+    this.route('posts');
+    this.route('followers');
+    this.route('following');
+  });
 });
