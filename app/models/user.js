b0447de6979c79ed7402c578d0a38c2b079397ab
David Chang <davidchchang@gmail.com>
2015-04-24 01:13:09 -0400

    Make recommended changes to data models. In users: 1) make 'id' the username, since parameters within routes are based off of ids and not other fields. 2) remove hashed password and salt; this is purely a back-end concept and it does not serve a purpose on the front-end 3) remove the hasMany relationships from the models, as this would cause all objects to be loaded at once instead of with pagination 4) createdAt is useful in the backend, but not required on the front-end Fixtures have been updated.

--- app/models/user.js
+++ app/models/user.js
@@ -2,73 +2,25 @@ import DS from 'ember-data';
 
 var User = DS.Model.extend({
   name: DS.attr('string'),
-  username: DS.attr('string'), // must be unique, see below to return as primaryKey
-  hashedPassword: DS.attr('string'),
-  salt: DS.attr('string'),
-
-  // doesn't seem like this is needed to create an account, but it is required for resetting the password
-  // https://www.dropbox.com/sh/3fq2wxngbugdm0y/AACZN_42lWtNJD19Vgc8fjnya?dl=0#lh:null-Create.png)
-  email: DS.attr('string'), // should be unique
-
-  // TODO: use custom queries to fetch the below
-  posts: DS.hasMany('post', {
-    inverse: 'owner'
-  }),
-  followers: DS.hasMany('user', {
-    inverse: 'following'
-  }),
-  following: DS.hasMany('user', {
-    inverse: 'followers'
-  }),
-
-  createdAt: DS.attr('string', {
-    defaultValue: function() { return new Date(); }
-  })
+  email: DS.attr('string') // should be unique
 });
-
-/*
- TODO: Use custom serializer to map primaryKey to username field
-
- App.UserSerializer = DS.JSONSerializer.extend({
-   primaryKey: 'username'
- });
-
- */
 
 User.reopenClass({
   FIXTURES: [
     {
-      id: 1,
+      id: "davidchchang",
       name: "David Chang",
-      username: "davidchchang",
-      hashedPassword: "asdf",
-      salt: "asdf",
-      email: "david@telegram.com",
-      posts: [1],
-      followers: [2, 3],
-      following: [2, 3]
+      email: "david@telegram.com"
     },
     {
-      id: 2,
+      id: "andreisoare",
       name: "Andrei Soare",
-      username: "andreisoare",
-      hashedPassword: "asdf",
-      salt: "asdf",
-      email: "andrei@telegram.com",
-      posts: [2, 3],
-      followers: [1, 3],
-      following: [1, 3]
+      email: "andrei@telegram.com"
     },
     {
-      id: 3,
+      id: "octavdruta",
       name: "Octav Druta",
-      username: "octavdruta",
-      hashedPassword: "asdf",
-      salt: "asdf",
-      email: "octav@telegram.com",
-      posts: [],
-      followers: [1, 2],
-      following: []
+      email: "octav@telegram.com"
     }
   ]
 });
