8027c6219699f081cdb7075ffe134fd18f141a3e
David Chang <davidchchang@gmail.com>
2015-04-24 01:14:59 -0400

    Make recommended changes to data models. In posts: - remove 'author' and replace with a link to the original post where the meta data can be extracted from. This will only be set if this is a re-post. Fixtures have been updated.

--- app/models/post.js
+++ app/models/post.js
@@ -4,38 +4,34 @@ var Post = DS.Model.extend({
   owner: DS.belongsTo('user', {
     async: true
   }),
-  author: DS.belongsTo('user', {
+  // originalPost will be set if this is a re-post
+  originalPost: DS.belongsTo('post', {
     async: true
   }),
   content: DS.attr('string'),
   timestamp: DS.attr('date')
-
-  // FOLLOWUP:
-  // - what happens when the original author deletes their post? do all reposts get deleted as well?
-  // - what happens if the original author no longer exists?
-  // - when a post is reposted, is the original timestamp displayed?
 });
 
 Post.reopenClass({
   FIXTURES: [
     {
       id: 1,
-      owner: 1,
-      author: 1,
+      owner: "davidchchang",
+      originalPost: null,
       content: "Spot on - https://www.youtube.com/watch?v=vVw1aPUybB8 #internet #party",
       timestamp: new Date("2015-03-24")
     },
     {
       id: 2,
-      owner: 2,
-      author: 1,
+      owner: "andreisoare",
+      originalPost: 1,
       content: "Spot on - https://www.youtube.com/watch?v=vVw1aPUybB8 #internet #party",
       timestamp: new Date("2015-03-25")
     },
     {
       id: 3,
-      owner: 2,
-      author: 2,
+      owner: "andreisoare",
+      originalPost: null,
       content: "My answer to What are the advantages of using Ember.js? http://qr.ae/qVycR",
       timestamp: new Date("2014-12-17")
     }
