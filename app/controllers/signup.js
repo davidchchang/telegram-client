8c74c3fb31b42f3f5ad8fab712fc6c53316dfb9d
David Chang <davidchchang@gmail.com>
2015-05-02 21:41:02 -0400

    Groundwork for authenticated sessions. If user is logged in, redirect from signup page to home page. Once user is successfully signed up, redirect to home page.

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,8 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  submitAction : function(){
-    // here you could perform your actions like persisting to the server or so
-    alert("now we can submit the model:" + this.get("model"));
+  needs: ['session'],
+  isLoggedIn: Ember.computed.alias('controllers.session.isAuthenticated'),
+
+  actions: {
+    signUp: function () {
+
+      var controller = this;
+      // Dependency injection provides the store object to the controller instance.
+      this.store.find('user').then(function (items) {
+        //controller.set('items', items);
+      });
+
+      var user = this.store.createRecord('user', {
+        id: this.get('model').id,
+        password: this.get('model').password,
+        operation: 'signup'
+      });
+
+      user.save().then(function (user) {
+        // Your user was saved. You can now redirect to the dashboard.
+
+        // There is an alias to the session property, so this change propagates
+        // to the session object then the IndexController.
+        this.set('isLoggedIn', true);
+
+        // Redirect to login
+        this.transitionToRoute('account.home');
+      }, function (response) {
+        // TODO Handle errors (E.g. if the user already exists)
+      });
+    }
   }
 });
--- app/routes/account/signup.js
+++ app/routes/account/signup.js
@@ -2,23 +2,16 @@ import Ember from 'ember';
 import User from '../../models/user.js';
 
 export default Ember.Route.extend({
+  controllerName: 'signup',
+
   model: function () {
-    var user = this.store.createRecord('user', {
-      id: '', // TODO: get this from controller
-      password: '',
-      operation: 'signup'
-    });
-
-    user.save().then(function(user) {
-      // Your user was saved. You can now redirect to the dashboard.
-    }, function(response) {
-      // Handle error
-    });
-
-    return user;
+    return this.store.createRecord('user');
   },
 
   setupController: function (controller, model) {
+    if (controller.get('isLoggedIn')) {
+      controller.transitionToRoute('account.home');
+    }
     controller.set("model", model);
   }
 });
