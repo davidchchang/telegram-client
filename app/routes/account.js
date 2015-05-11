9fab4788d26c132329940507187b483464a2b1d2
David Chang <davidchchang@gmail.com>
2015-05-11 14:54:35 -0400

    Redirect to login page if not authenticated and user is on the index page. All other account pages should be rendered.

--- app/routes/account.js
+++ app/routes/account.js
@@ -4,6 +4,9 @@ export default Ember.Route.extend({
   beforeModel: function(transition) {
     if (this.controllerFor('session').get('isAuthenticated')) {
       this.transitionTo('account.home');
+    } else if (transition.targetName === 'account.index') {
+      // not logged in
+      this.transitionTo('account.login');
     }
   }
 });
